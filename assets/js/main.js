1637154082ca245eba58712a38f5e15c664fc68e
sightsandsnaps <sightsandsnaps@gmail.com>
2018-07-07 20:21:59 -0400

    deleted old files

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript" src="dist/fullpage/js/fullpage.min.js"> </script>
-    <script type="text/javascript">
-		$(document).ready(function () {
-			$('#main').fullpage({
-				anchors: ['ONE', 'TWO'],
-			});
-		});
-    </script>
